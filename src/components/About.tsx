6c71369b9cc8b45eb9e585352c156aba5e48bf66
MuhibAnwar <muhibanwar065@gmail.com>
2024-11-11 19:59:55 +0500

    change in About file

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -28,8 +28,20 @@ const About = () => {
         }}
         data-aos="zoom-in-up"
       >
-        Hi there! I am a passionate Computer Science student with a deep interest in technology, programming, and problem-solving. I am always eager to learn new skills, explore innovative ideas, and apply my knowledge in real-world projects. Currently enrolled in GIAIC, I have honed my technical skills in areas like software development, algorithms, and data structures. My journey began with a solid foundation in Matriculation from BSEK, and now I am continuing my studies in the intermediate level at D.J. Science Sindh Govt. College. If you are looking for a dedicated and driven individual to collaborate with or work on exciting projects in the tech field, I do love to connect and discuss potential opportunities. Let's create something amazing together
+        Hi there, I am a passionate Computer Science
+         student with a deep interest in technology, programming, 
+         and problem-solving. I am always eager to learn new skills, explore innovative ideas, 
+         and apply my knowledge in real-world projects. Currently enrolled in GIAIC, I have honed my 
+         technical skills in areas like software development, algorithms, and data structures. My journey
+          began with a solid foundation in Matriculation from BSEK, and now I am continuing my studies 
+          in the intermediate level at D.J. Science Sindh Govt. College. If you are looking for 
+          a dedicated and driven individual to collaborate with or work on exciting projects in 
+          the tech field, I do love to connect and discuss potential opportunities. Let
+          
+           create 
+          something amazing together
       </p>
+
     </div>
   );
 };
